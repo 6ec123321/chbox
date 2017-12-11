2f07dfb53ca39a7ae01dc32ef48954f4a995952b
Manuel Delgado <manuel.delgado@ucr.ac.cr>
2017-12-11 08:13:43 -0600

    Separates chbox_menu and list options into skin specific JS Works with RC 1.3.3 Separates chbox logic from presentation creating a skin specific JS to control how list options and menu are displayed on each skin

--- chbox.js
+++ chbox.js
@@ -40,11 +40,6 @@ function rcmail_selectmenu() {
   return false;
 }
 
-function chbox_menu(){
-  var link_html = '<a href="#" onclick="return rcmail.command(\'plugin.chbox.selectmenu\')">'+rcmail.env.chboxicon;
-  $('#rcmchbox').html(link_html);
-}
-
 if (window.rcmail) {
   rcmail.addEventListener('init', function(evt) {
     rcmail.register_command('plugin.chbox.selectmenu', rcmail_selectmenu, true);
@@ -98,10 +93,3 @@ if (window.rcmail) {
   });
 }
 
-
-$(document).ready(function(){
-  chbox_menu();
-    var li = '<label class="disabled"><input type="checkbox" name="list_col[]" value="chbox" name="cols_chbox" checked="checked" disabled="disabled" type="checkbox"/><span>'+rcmail.get_label('chbox.chbox')+'</span></label>';
-  $("#listmenu fieldset ul input#cols_threads").parent().after(li);
-  $("#listoptions fieldset ul.proplist:first li:first-child").after('<li>'+li+'</li>');
-});
