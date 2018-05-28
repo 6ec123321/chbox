3466326c99d5a79acbb27a68580c177964302283
Artur Petrov <artur@phpchain.ru>
2018-05-28 23:06:20 +0400

    Update chbox.js

--- skins/classic/chbox.js
+++ skins/classic/chbox.js
@@ -6,6 +6,6 @@ function chbox_menu(){
 
 $(document).ready(function(){
   chbox_menu();
-    var li = '<label class="disabled"><input type="checkbox" name="list_col[]" value="chbox" id="cols_chbox" checked="checked" disabled="disabled" type="checkbox"/><span>'+rcmail.get_label('chbox.chbox')+'</span></label>';
+    var li = '<label><input type="checkbox" name="list_col[]" value="chbox" id="cols_chbox" checked="checked" type="checkbox"/><span>'+rcmail.get_label('chbox.chbox')+'</span></label>';
   $("#listmenu ul.toolbarmenu li:first()").after('<li>'+li+'</li>');
 });
